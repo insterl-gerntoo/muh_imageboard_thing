fef5f8516247afb78600d6ca9e9baa0b5c088eb7
bla <insterl_gerntoo@gmx.com>
2015-01-08 14:31:14 -0600

    Added comparator function for stdlib's qsort

--- server/id_math.h
+++ server/id_math.h
@@ -7,6 +7,7 @@ typedef struct {
     uint8_t bytes[16];
 } ID;
 
+ID get_id();
 void get_id_as_hex_string(ID, char *dest);
 ID parse_id_from_hex_string(char *src);
 ID get_max_ID();
@@ -14,6 +15,7 @@ ID id_add(ID id_1, ID id_2);
 ID id_subtract(ID id_1, ID id_2);
 ID id_bit_shift_right(ID id, int num_bits);
 int id_compare(ID id_1, ID id_2);
+int id_comparator(const void *a, const void *b);
 ID idDistance(ID id_1, ID id_2);
 
 #endif
